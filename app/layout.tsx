11196044e423c45832410866981da38cb63e5c13
Hollie Duncan <hollie@contento.io>
2024-10-22 17:09:59 +0100

    removes notFound from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,28 +20,20 @@ export default async function RootLayout({
   const mainNavId = process.env.SITE_MAIN_NAV_ID ?? false
   const footerNavId = process.env.SITE_FOOTER_NAV_ID ?? false
 
-  if (!mainNavId) {
-    throw new Error(
-      'No main nav found. Please ensure you have created one in Contento and copied the ID to your .env file.',
-    )
-  }
-
-  if (!footerNavId) {
-    throw new Error(
-      'No footer nav found. Please ensure you have created one in Contento and copied the ID to your .env file.',
-    )
-  }
-
   const mainNav = await createClient()
     .getContentById(mainNavId)
     .catch(() => {
-      notFound()
+      throw new Error(
+        'No main nav found. Please ensure you have created one in Contento and copied the ID to your .env file.',
+      )
     })
 
   const footerNav = await createClient()
     .getContentById(footerNavId)
     .catch(() => {
-      notFound()
+      throw new Error(
+        'No footer nav found. Please ensure you have created one in Contento and copied the ID to your .env file.',
+      )
     })
 
   return (
