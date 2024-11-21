0e5d792e7cb245f924488cc905bc3c9ceb347e17
Hollie Duncan <hollie@contento.io>
2024-11-21 14:45:59 +0000

    Logo update and style adjustments

--- components/blocks/ImageAndText.tsx
+++ components/blocks/ImageAndText.tsx
@@ -3,20 +3,21 @@ import Image from 'next/image'
 import Button from './Button'
 
 export default function ImageAndText({ block }: { block: BlockData }) {
-  const image = block.fields.image.assets.length > 0 ? (
-    <div>
-      <Image
-        src={block.fields.image.assets[0].asset.url}
-        alt={block.fields.image.assets[0].asset.description}
-        className="h-full w-full object-cover"
-        width={176}
-        height={176}
-      />
-    </div>
-  ) : null
+  const image =
+    block.fields.image.assets.length > 0 ? (
+      <div>
+        <Image
+          src={block.fields.image.assets[0].asset.url}
+          alt={block.fields.image.assets[0].asset.description}
+          className="h-full w-full object-cover"
+          width={176}
+          height={176}
+        />
+      </div>
+    ) : null
   return (
     <div className="py-9 md:py-16">
-      <div className="grid items-center space-y-6 md:grid-cols-2 md:space-x-12">
+      <div className="grid items-center space-y-9 lg:grid-cols-2 lg:space-x-12">
         {block.fields.image_side.selected_option.value === 'left' && (
           <>{image}</>
         )}
